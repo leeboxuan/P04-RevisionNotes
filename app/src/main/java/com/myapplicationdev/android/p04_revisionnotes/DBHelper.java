079b468e9b60a4fa960df0ea113c7db262d35b18
16046473 <leebouan@hotmail.com>
2018-05-14 15:00:22 +0800

    Done problem statement and updated

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/DBHelper.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/DBHelper.java
@@ -12,9 +12,13 @@ import java.util.ArrayList;
 public class DBHelper extends SQLiteOpenHelper {
 
 	//TODO Define the Database properties
-	private static final String DATABASE_NAME = "";
-	private static final int DATABASE_VERSION = ;
+	private static final String DATABASE_NAME = "notes.db";
+	private static final int DATABASE_VERSION = 1;
 
+	private static final String TABLE_NOTE = "note";
+	private static final String COLUMN_ID = "id";
+	private static final String COLUMN_NOTECONTENT = "noteContent";
+	private static final String COLUMN_STARS = "stars";
 
 	public DBHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -23,6 +27,12 @@ public class DBHelper extends SQLiteOpenHelper {
 	@Override
 	public void onCreate(SQLiteDatabase db) {
 		//TODO CREATE TABLE Note
+		String createTableSq1 = "CREATE TABLE " + TABLE_NOTE + " ( "
+				+ COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+				+ COLUMN_NOTECONTENT + " TEXT, "
+				+ COLUMN_STARS + " INTEGER )";
+
+		db.execSQL(createTableSq1);
 
 	}
 
@@ -34,37 +44,66 @@ public class DBHelper extends SQLiteOpenHelper {
 
 	public void insertNote(String noteContent, int stars) {
 		//TODO insert the data into the database
+		SQLiteDatabase db = this.getWritableDatabase();
+		ContentValues value = new ContentValues();
+		value.put(COLUMN_NOTECONTENT, noteContent);
+		value.put(COLUMN_STARS, stars);
+		db.insert(TABLE_NOTE, null, value);
+		db.close();
 	}
 
 	public ArrayList<Note> getAllNotes() {
 		//TODO return records in Java objects
+		ArrayList<Note> notes = new ArrayList<Note>();
+		String selectQuery = "SELECT " + COLUMN_ID + ", "
+				+ COLUMN_NOTECONTENT + ", "
+				+ COLUMN_STARS
+				+ " FROM " + TABLE_NOTE;
+		SQLiteDatabase db = this.getReadableDatabase();
+		Cursor cursor = db.rawQuery(selectQuery,null);
+
+		if(cursor.moveToFirst()){
+			do{
+				int id = cursor.getInt(0);
+				String noteContent = cursor.getString(1);
+				int stars = cursor.getInt(2);
+				Note obj = new Note(id, noteContent, stars);
+				notes.add(obj);
+			} while (cursor.moveToNext());
+		}
+		cursor.close();
+		db.close();
+
+		return notes;
 	}
 
-    public ArrayList<String> getNoteContent() {
-        //TODO return records in Strings
+	public ArrayList<String> getNoteContent() {
+		//TODO return records in Strings
 
 		// Create an ArrayList that holds String objects
-        ArrayList<String> notes = new ArrayList<String>();
-        // Select all the notes' content
-        String selectQuery = "";
+		ArrayList<String> notes = new ArrayList<String>();
+		// Select all the notes' content
+		String selectQuery = "SELECT " + COLUMN_NOTECONTENT
+				+ " FROM " + TABLE_NOTE;
 
-        // Get the instance of database to read
-        SQLiteDatabase db = this.getReadableDatabase();
-        // Run the SQL query and get back the Cursor object
-        Cursor cursor = db.rawQuery(selectQuery, null);
-        // moveToFirst() moves to first row
-        if (cursor.moveToFirst()) {
-            // Loop while moveToNext() points to next row and returns true;
-            // moveToNext() returns false when no more next row to move to
-            do {
+		// Get the instance of database to read
+		SQLiteDatabase db = this.getReadableDatabase();
+		// Run the SQL query and get back the Cursor object
+		Cursor cursor = db.rawQuery(selectQuery, null);
+		// moveToFirst() moves to first row
+		if (cursor.moveToFirst()) {
+			// Loop while moveToNext() points to next row and returns true;
+			// moveToNext() returns false when no more next row to move to
+			do {
 
+				notes.add(cursor.getString(0));
 
-            } while (cursor.moveToNext());
-        }
-        // Close connection
-        cursor.close();
-        db.close();
+			} while (cursor.moveToNext());
+		}
+		// Close connection
+		cursor.close();
+		db.close();
 
-        return notes;
-    }
+		return notes;
+	}
 }
